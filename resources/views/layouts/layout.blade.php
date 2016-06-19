a31e2f8e015bd01585422fdd3e15fd04fbdc677d
Niall Brennan <niallbrennan87@gmail.com>
2016-06-19 17:08:49 +0100

    update csrf

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
 
 <html lang="en">
     <head>
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
